6f25feeee715d56b03d098315fb5fb2c8f0b3d51
Guillaume Riby <guillaumeriby@gmail.com>
2019-08-15 20:29:00 -0400

    Chapter 4: Adding a sphere
    Add a sphere as described in Chapter 4.

--- ray-tracing-series/src/main.cpp
+++ ray-tracing-series/src/main.cpp
@@ -11,8 +11,41 @@ using namespace rts;
 
 const std::string IMAGE_FILE_PATH = "output/image.ppm";
 
+// to determine if a ray hits a sphere consider this,
+// any point P that's on a sphere of radius R and center C must satisfy the following equation:
+//      (Px-Cx)*(Px-Cx) + (Py-Cy)*(Py-Cy) + (Pz-Cz)*(Pz-Cz) = R*R
+// which happens to be:
+//      dot(P-C, P-C) = R*R
+// we want to determine if the ray (P(t) = A + t*B) ever hits the sphere,
+// in other words, if there's a point on the ray which satisfies this equation:
+//      dot(P(t)-C, P(t)-C) = R*R
+//      dot(A+t*B-C, A+t*B-C) = R*R
+// which can be rearranged into the following quadratic equation:
+//      t*t*dot(B, B) + 2*t*dot(B, A-C) + dot(A-C, A-C) - R*R = 0
+bool hitSphere(const vec3& center, float radius, const ray& r)
+{
+    // solve the quadratic equation (a*x^2 + b*x + c = 0)
+    //      t*t*dot(B, B) + 2*t*dot(B, A-C) + dot(A-C, A-C) - R*R = 0
+    // where A is the ray's origin and B its direction, C is the center of the sphere and R its radius
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.f * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+
+    // if the discriminant if greater than zero the ray hits the sphere (twice)
+    // at zero the ray is only tangent to the sphere (it hits it once)
+    return discriminant > 0;
+}
+
 vec3 color(const ray& r)
 {
+    // check if the ray hits a sphere at the center of the screen
+    if (hitSphere(vec3(0.f, 0.f, -1.f), 0.5f, r))
+    {
+        return vec3(1.f, 0.f, 0.f);
+    }
+
     // unitDirection Y is between -1 and +1
     vec3 unitDirection = unitVector(r.direction());
 
