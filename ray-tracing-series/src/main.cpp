d614b3d1eec0a2818f6fece8a30b64a544d48024
Guillaume Riby <guillaumeriby@gmail.com>
2019-08-17 15:33:58 -0400

    Extract the ray tracing and write to file code in 2 functions
    The ray tracing part goes to performRayTracing, the write to file part goes to writeImageFile.

--- ray-tracing-series/src/main.cpp
+++ ray-tracing-series/src/main.cpp
@@ -60,6 +60,75 @@ namespace rts // for ray tracing series
             return (1.f - t) * vec3(1.f, 1.f, 1.f) + t * vec3(0.5f, 0.7f, 1.f);
         }
     }
+
+    // Declare aliases for the image data
+    using Color = std::tuple<int, int, int>;
+    using ImageData = std::array<Color, IMAGE_WIDTH* IMAGE_HEIGHT>;
+
+    template<int N>
+    void performRayTracing(const Camera& camera, const HitableList<N>& world, ImageData* imageData)
+    {
+        // Run the ray tracer on each pixel to determine its color
+        // from left to right and top to bottom
+        for (int j = IMAGE_HEIGHT - 1; j >= 0; --j)
+        {
+            for (int i = 0; i < IMAGE_WIDTH; ++i)
+            {
+                vec3 col(0.f, 0.f, 0.f); // the accumulated color
+
+                // Sample multiple times randomly within the current pixel
+                for (int s = 0; s < SAMPLES_PER_PIXEL; ++s)
+                {
+                    float u = float(i + random.get()) / float(IMAGE_WIDTH);
+                    float v = float(j + random.get()) / float(IMAGE_HEIGHT);
+                    Ray r = camera.getRay(u, v);
+
+                    // Accumulate the sample
+                    col += color(r, world);
+                }
+
+                // Average the color
+                col /= static_cast<float>(SAMPLES_PER_PIXEL);
+
+                // Apply a gamma to brighten the color
+                col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+
+                // Scale the color between 0 and 255
+                int ir = int(255.99f * col[0]);
+                int ig = int(255.99f * col[1]);
+                int ib = int(255.99f * col[2]);
+
+                // Store the resulting color in the array
+                (*imageData)[i + j * IMAGE_WIDTH] = std::make_tuple(ir, ig, ib);
+            }
+        }
+    }
+
+    void writeImageFile(const ImageData* imageData)
+    {
+        // Write the ray tracer output to the image file
+        std::ofstream imageFile(IMAGE_FILE_PATH);
+        if (imageFile.is_open())
+        {
+            // Write the image file header
+            imageFile << "P3" << std::endl;
+            imageFile << IMAGE_WIDTH << " " << IMAGE_HEIGHT << std::endl;
+            imageFile << "255" << std::endl;
+
+            // Iterate over the ray tracer output
+            for (int j = IMAGE_HEIGHT - 1; j >= 0; --j)
+            {
+                for (int i = 0; i < IMAGE_WIDTH; ++i)
+                {
+                    // Output the color to the image file
+                    auto col = (*imageData)[i + j * IMAGE_WIDTH];
+                    imageFile << std::get<0>(col) << " " << std::get<1>(col) << " " << std::get<2>(col) << std::endl;
+                }
+            }
+
+            imageFile.close();
+        }
+    }
 }
 
 int main()
@@ -75,70 +144,16 @@ int main()
     HitableList<2> world;
     world[0] = std::move(std::make_unique<Sphere>(vec3(0.f, 0.f, -1.f), 0.5f));         // sphere at the center of the screen
     world[1] = std::move(std::make_unique<Sphere>(vec3(0.f, -100.5f, -1.f), 100.f));    // sphere representing the ground
-    Camera cam(IMAGE_ASPECT_RATIO);
+    Camera camera(IMAGE_ASPECT_RATIO);
 
     // The array which will store the colors output by the ray tracer
-    using Color = std::tuple<int, int, int>;
-    using ImageData = std::array<Color, IMAGE_WIDTH* IMAGE_HEIGHT>;
     auto imageData = std::make_unique<ImageData>();
 
-    // Run the ray tracer on each pixel to determine its color
-    // from left to right and top to bottom
-    for (int j = IMAGE_HEIGHT - 1; j >= 0; --j)
-    {
-        for (int i = 0; i < IMAGE_WIDTH; ++i)
-        {
-            vec3 col(0.f, 0.f, 0.f); // the accumulated color
+    // Ray tracing
+    performRayTracing(camera, world, imageData.get());
 
-            // Sample multiple times randomly within the current pixel
-            for (int s = 0; s < SAMPLES_PER_PIXEL; ++s)
-            {
-                float u = float(i + random.get()) / float(IMAGE_WIDTH);
-                float v = float(j + random.get()) / float(IMAGE_HEIGHT);
-                Ray r = cam.getRay(u, v);
-
-                // Accumulate the sample
-                col += color(r, world);
-            }
-
-            // Average the color
-            col /= static_cast<float>(SAMPLES_PER_PIXEL);
-
-            // Apply a gamma to brighten the color
-            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
-
-            // Scale the color between 0 and 255
-            int ir = int(255.99f * col[0]);
-            int ig = int(255.99f * col[1]);
-            int ib = int(255.99f * col[2]);
-
-            // Store the resulting color in the array
-            (*imageData)[i + j * IMAGE_WIDTH] = std::make_tuple(ir, ig, ib);
-        }
-    }
-
-    // Write the ray tracer output to the image file
-    std::ofstream imageFile(IMAGE_FILE_PATH);
-    if (imageFile.is_open())
-    {
-        // Write the image file header
-        imageFile << "P3" << std::endl;
-        imageFile << IMAGE_WIDTH << " " << IMAGE_HEIGHT << std::endl;
-        imageFile << "255" << std::endl;
-
-        // Iterate over the ray tracer output
-        for (int j = IMAGE_HEIGHT - 1; j >= 0; --j)
-        {
-            for (int i = 0; i < IMAGE_WIDTH; ++i)
-            {
-                // Output the color to the image file
-                auto col = (*imageData)[i + j * IMAGE_WIDTH];
-                imageFile << std::get<0>(col) << " " << std::get<1>(col) << " " << std::get<2>(col) << std::endl;
-            }
-        }
-
-        imageFile.close();
-    }
+    // Output to file
+    writeImageFile(imageData.get());
 
     // Compute and display the elapsed time
     auto endTime = std::chrono::system_clock::now();
