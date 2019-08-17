0acdba8bd40db09af7d69b91ac4eb0babf5b38ba
Guillaume Riby <guillaumeriby@gmail.com>
2019-08-17 12:35:56 -0400

    Move the configuration variables in a config header

--- ray-tracing-series/src/main.cpp
+++ ray-tracing-series/src/main.cpp
@@ -10,30 +10,9 @@
 #include "hitableList.h"
 #include "camera.h"
 #include "random.h"
+#include "config.h"
 
 using namespace rts;
-
-
-// The configuration defines as set in the preprocessor definitions:
-// Project Properties > C/C++ > Preprocessor > Preprocessor Definitions
-//  * DETERMINISTIC_EXECUTION=1
-//  * NORMAL_MAP_COLOR=0
-
-// Image output path
-const std::string IMAGE_FILE_PATH = "output/image.ppm";
-
-// Image resolution
-const int IMAGE_WIDTH = 200;
-const int IMAGE_HEIGHT = 100;
-const float IMAGE_ASPECT_RATIO = static_cast<float>(IMAGE_WIDTH) / IMAGE_HEIGHT;
-
-// Sampling per pixel
-const int SAMPLES_PER_PIXEL = 100;
-
-// The shortest and longest ray length
-const float T_MIN = 0.001f;
-const float T_MAX = std::numeric_limits<float>::max();
-
 
 // Initialize a global random value generator
 Random random;
