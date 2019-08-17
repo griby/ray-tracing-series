343a434c64ef19a7552cb061d932b97bd9f5058a
Guillaume Riby <guillaumeriby@gmail.com>
2019-08-17 12:38:07 -0400

    Remove global using namespace directive

--- ray-tracing-series/src/main.cpp
+++ ray-tracing-series/src/main.cpp
@@ -12,55 +12,58 @@
 #include "random.h"
 #include "config.h"
 
-using namespace rts;
-
-// Initialize a global random value generator
-Random random;
-
-vec3 randomPointInUnitSphere()
+namespace rts // for ray tracing series
 {
-    vec3 p;
-    do
+    // Initialize a global random value generator
+    Random random;
+
+    vec3 randomPointInUnitSphere()
     {
-        // Generate a random point in a unit cube ie its components fall between -1 and +1
-        p = 2.f * vec3(random.get(), random.get(), random.get()) - vec3(1.f, 1.f, 1.f);
+        vec3 p;
+        do
+        {
+            // Generate a random point in a unit cube ie its components fall between -1 and +1
+            p = 2.f * vec3(random.get(), random.get(), random.get()) - vec3(1.f, 1.f, 1.f);
 
-        // Until we find one that is contained in the unit sphere
-    } while (p.squaredLength() >= 1.f);
+            // Until we find one that is contained in the unit sphere
+        } while (p.squaredLength() >= 1.f);
 
-    return p;
-}
-
-template <int N>
-vec3 color(const Ray& r, const HitableList<N>& world)
-{
-    // Check if the ray hits a sphere at the center of the screen
-    HitRecord rec;
-    if (world.hit(r, T_MIN, T_MAX, rec))
-    {
-#if NORMAL_MAP_COLOR
-        // The normal is a unit vector ie its components fall between -1 and +1
-        // map those components between 0 and +1 before returning the value
-        return 0.5f * vec3(rec.normal.x() + 1.f, rec.normal.y() + 1.f, rec.normal.z() + 1.f);
-#else
-        // The ray hit a surface, determine a new target to bounce off the surface
-        vec3 target = rec.p + rec.normal + randomPointInUnitSphere();
-        return 0.5f * color(Ray(rec.p, target - rec.p), world);
-#endif
+        return p;
     }
-    else
-    {
-        // Nothing has been hit, determine the background's color
-        vec3 unitDirection = unitVector(r.direction());     // unitDirection Y is between -1 and +1
-        float t = 0.5f * (unitDirection.y() + 1.f);         // scale unitDirection Y between 0 and +1
 
-        // Blend between white and blue depending on the direction Y
-        return (1.f - t) * vec3(1.f, 1.f, 1.f) + t * vec3(0.5f, 0.7f, 1.f);
+    template <int N>
+    vec3 color(const Ray& r, const HitableList<N>& world)
+    {
+        // Check if the ray hits a sphere at the center of the screen
+        HitRecord rec;
+        if (world.hit(r, T_MIN, T_MAX, rec))
+        {
+#if NORMAL_MAP_COLOR
+            // The normal is a unit vector ie its components fall between -1 and +1
+            // map those components between 0 and +1 before returning the value
+            return 0.5f * vec3(rec.normal.x() + 1.f, rec.normal.y() + 1.f, rec.normal.z() + 1.f);
+#else
+            // The ray hit a surface, determine a new target to bounce off the surface
+            vec3 target = rec.p + rec.normal + randomPointInUnitSphere();
+            return 0.5f * color(Ray(rec.p, target - rec.p), world);
+#endif
+        }
+        else
+        {
+            // Nothing has been hit, determine the background's color
+            vec3 unitDirection = unitVector(r.direction());     // unitDirection Y is between -1 and +1
+            float t = 0.5f * (unitDirection.y() + 1.f);         // scale unitDirection Y between 0 and +1
+
+            // Blend between white and blue depending on the direction Y
+            return (1.f - t) * vec3(1.f, 1.f, 1.f) + t * vec3(0.5f, 0.7f, 1.f);
+        }
     }
 }
 
 int main()
 {
+    using namespace rts;
+
     std::ofstream imageFile(IMAGE_FILE_PATH);
     if (imageFile.is_open())
     {
