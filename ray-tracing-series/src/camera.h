b0f73a98ad7804f6b849cf49f79c6045035d5df8
Guillaume Riby <guillaumeriby@gmail.com>
2019-08-20 01:57:12 -0400

    Class members are prefixed by "m_" | Camera

--- ray-tracing-series/src/camera.h
+++ ray-tracing-series/src/camera.h
@@ -8,21 +8,21 @@ namespace rts // for ray tracing series
     {
     public:
         Camera(float aspectRatio)
-            : origin(0.f, 0.f, 0.f)
-            , lowerLeftCorner(-2.f, -2.f/aspectRatio, -1.f)
-            , horizontal(4.f, 0.f, 0.f)
-            , vertical(0.f, 4.f/aspectRatio, 0.f)
+            : m_origin(0.f, 0.f, 0.f)
+            , m_lowerLeftCorner(-2.f, -2.f/aspectRatio, -1.f)
+            , m_horizontal(4.f, 0.f, 0.f)
+            , m_vertical(0.f, 4.f/aspectRatio, 0.f)
         {
         }
 
         // Return the ray starting at the camera position and oriented towards a specific point in space
         // this point is determined by applying the given offset to the arbitrary position lowerLeftCorner
-        Ray getRay(float u, float v) const { return Ray(origin, lowerLeftCorner + u * horizontal + v * vertical - origin); }
+        Ray getRay(float u, float v) const { return Ray(m_origin, m_lowerLeftCorner + u * m_horizontal + v * m_vertical - m_origin); }
 
     private:
-        vec3 origin;
-        vec3 lowerLeftCorner;
-        vec3 horizontal;
-        vec3 vertical;
+        vec3 m_origin;
+        vec3 m_lowerLeftCorner;
+        vec3 m_horizontal;
+        vec3 m_vertical;
     };
 }
