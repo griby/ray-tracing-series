49d7beb5ec9ec67af01104c4cf4c633d1f8d362a
Guillaume Riby <guillaumeriby@gmail.com>
2019-08-20 01:32:12 -0400

    Chapter 9: Dielectrics (final pass)
    Unfortunately the previous fix for the cosine computation introduces NaNs. That's because the fix involves the square root of a discriminant which can be negative. When it's negative it means that there's total internal reflection.
    At the same time rearrange the dielectric scattering implementation to avoid the premature computation of the cosine value.

--- ray-tracing-series/src/dielectric.cpp
+++ ray-tracing-series/src/dielectric.cpp
@@ -23,40 +23,49 @@ namespace rts
         {
             outwardNormal = -rec.normal;
             niOverNt = refIdx;
-
-            // Previous computation of cosine as described in the book (it's bugged!)
-            //cosine = refIdx * dt / rIn.direction().length();
-
-            // Compute the cosine to pass to Schlick's approximation function as fixed by the following post
-            // http://psgraphics.blogspot.com/2016/03/my-buggy-implimentation-of-schlick.html
-
-            // it's the cosine of the greatest of the 2 angles involved in the refraction, regardless of which direction the ray is travelling in
-            // in this case the ray is travelling from inside the sphere and towards the outside,
-            // the angle of exit will be greater since air's refraction index is lower than the material's
-
-            // TODO instead of normalizing here, we should get a unit vector out of rIn.direction() and pass it to getRefracted
-            cosine = dt / rIn.direction().length(); // this is the cosine of the incoming angle (the smallest of the 2 angles)
-            cosine = sqrt(1.f - refIdx * refIdx * (1.f - cosine * cosine));
         }
         else
         {
             outwardNormal = rec.normal;
             niOverNt = 1.f / refIdx;
-
-            // TODO instead of normalizing here, we should get a unit vector out of rIn.direction() and pass it to getRefracted
-            cosine = -dt / rIn.direction().length();
         }
 
         // Determine the reflection probability
         vec3 refracted;
-        float reflectProb;
-        if (getRefracted(rIn.direction(), outwardNormal, niOverNt, refracted)) // TODO call getRefracted before computing the cosine, we may have computed it for nothing
+        float reflectProb = 1.f;
+        if (getRefracted(rIn.direction(), outwardNormal, niOverNt, refracted))
         {
-            reflectProb = getSchlick(cosine, refIdx);
-        }
-        else
-        {
-            reflectProb = 1.f;
+            // Compute cosine here, where it's needed
+            if (dt > 0.f)
+            {
+                // Previous computation of cosine as described in the book (it's bugged!)
+                //cosine = refIdx * dt / rIn.direction().length();
+
+                // Compute the cosine to pass to Schlick's approximation function as fixed by the following post
+                // http://psgraphics.blogspot.com/2016/03/my-buggy-implimentation-of-schlick.html
+
+                // it's the cosine of the greatest of the 2 angles involved in the refraction, regardless of which direction the ray is travelling in
+                // in this case the ray is travelling from inside the sphere and towards the outside,
+                // the angle of exit will be greater since air's refraction index is lower than the material's
+
+                // TODO instead of normalizing here, we should get a unit vector out of rIn.direction() and pass it to getRefracted
+                cosine = dt / rIn.direction().length(); // this is the cosine of the incoming angle (the smallest of the 2 angles)
+                float discriminant = 1.f - refIdx * refIdx * (1.f - cosine * cosine);
+
+                // Proceed with the Schlick approximation only if the discriminant is positive
+                // when it's negative it means that there's total internal reflection
+                if (discriminant > 0.f)
+                {
+                    cosine = sqrt(discriminant);
+                    reflectProb = getSchlick(cosine, refIdx);
+                }
+            }
+            else
+            {
+                // TODO instead of normalizing here, we should get a unit vector out of rIn.direction() and pass it to getRefracted
+                cosine = -dt / rIn.direction().length();
+                reflectProb = getSchlick(cosine, refIdx);
+            }
         }
 
         // Reflect or refract depending on the reflection probability
